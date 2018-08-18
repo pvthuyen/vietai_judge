3c920a50c3ab54ef6eab6afc1a250191c55b3025
Thuyen Phan <pvthuyen@apcs.vn>
2018-08-19 00:33:16 +0700

    add a small message to returned score

--- judge/views.py
+++ judge/views.py
@@ -15,19 +15,21 @@ def evaluate_submission(submitted_file, judge_file):
         result = subprocess.run('python3 {} {}'.format(judge_file, submitted_file), shell=True, timeout=2.5, check=True, stdout=subprocess.PIPE)
     except Exception as e:
         return 0
-        
-    return int(result.stdout)
+    result_lines = result.stdout.split('\n', 1)
+    return int(result_lines[0]), result_lines[1]
 
 @csrf_exempt
 @require_http_methods(['POST'])
 def upload_file(request, assignment_id):
+    print(request.body)
     assignment = get_object_or_404(Assignment, id=assignment_id)
     submission = Submission.objects.create(uploaded_file=request.FILES['file'], assignment=assignment)
-    score = evaluate_submission(settings.MEDIA_ROOT + submission.uploaded_file.name, settings.MEDIA_ROOT + assignment.judge_file.name)
+    score, message = evaluate_submission(settings.MEDIA_ROOT + submission.uploaded_file.name, settings.MEDIA_ROOT + assignment.judge_file.name)
     submission.score = score
     submission.save()
-    return JsonResponse({ 'score': score })
+    return JsonResponse({ 'score': score, 'message': message })
 
 @login_required
 def index(request):
-    return render(request, 'index.html')
+    assignments = Assignment.objects.all()
+    return render(request, 'index.html', { 'assignments': assignments })
