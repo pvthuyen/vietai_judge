289b885c74ddc89604bf94b49d263a12a05d5409
Thuyen Phan <pvthuyen@apcs.vn>
2018-09-03 01:37:34 +0700

    add created_date and modified_date

--- judge/models.py
+++ judge/models.py
@@ -7,6 +7,8 @@ class Assignment(models.Model):
     judge_file = models.FileField(upload_to='judge/')
 
 class Submission(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    modified_date = models.DateTimeField(auto_now=True, null=True)
     # file will be uploaded to MEDIA_ROOT/uploads
     uploaded_file = models.FileField(upload_to='submissions/')
     assignment = models.ForeignKey(Assignment, db_index=True, null=True, related_name='submissions', on_delete=models.CASCADE)
